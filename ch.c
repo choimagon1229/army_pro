e99b1549c6508f625ff5716b02f3c23e1ad3c597
choimagon1229 <66016075+choimagon1229@users.noreply.github.com>
2021-07-25 22:37:28 +0900

    게임 캐릭터 문자로 초안
    게임 캐릭터 문자로 만들어봤습니다.

---
+++ ch.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main()
+{
+    //전사 - 궁수 - 방패- 마법사 
+    //전사
+    printf("\n");
+    printf("전사\n\n\n");
+    printf("\t\\ R\n");
+    printf("\t \\()\n");
+    printf("\t  Il\n");
+    printf("===========================\n");
+    printf("\n");
+    //궁수
+    printf("\n");
+    printf("궁수\n\n\n");
+    printf("\t / Q\n");
+    printf("\tㅓ ()\n");
+    printf("\t \\ Il\n");
+    printf("===========================\n");
+    printf("\n");
+    //방패
+    printf("\n");
+    printf("방패\n\n\n");
+    printf("\tI Q\n");
+    printf("\tL D)\n");
+    printf("\tI Il\n");
+    printf("===========================\n");
+    //마법사
+    printf("\n");
+    printf("마법사\n\n\n");
+    printf("\t0 R\n");
+    printf("\tL D\\\n");
+    printf("\tI Il\n");
+    printf("===========================\n");
+    printf("\n");
+
+    return 0;
+}
+
